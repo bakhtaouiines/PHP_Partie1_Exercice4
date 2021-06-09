e12aa772395c4c340f82c79bf73645cd2b3dffd2
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 14:53:59 +0200

    correction ajout HTML

--- exercice1.4.php
+++ exercice1.4.php
@@ -1,7 +1,21 @@
-<?php
-$string = "ma variable de type string";
-$int = 20 ;
-$float = 12.5 ;
-$boolean = true;
-echo $string.',<br> ma variable de type integer: ' .$int. ',<br> ma variable de type float: ' .$float. ',<br> ma variable de type boolean: ' .$boolean;
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1.4</title>
+</head>
+<body>
+    <h1>Exercice 4 Partie 1 : Les variables PHP</h1>
+    <p>
+        <?php
+            $string = 'ma variable de type string';
+            $int = 20 ;
+            $float = 12.5 ;
+            $boolean = true;
+            echo $string. nl2br("\n ma variable de type integer: ") .$int. nl2br("\n ma variable de type float: ") .$float. nl2br("\n ma variable de type boolean: ") .$boolean;
+        ?>
+    </p>
+</body>
+</html>
