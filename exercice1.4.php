5e7198a566e45dabe37c8b92235a4251d224bcbf
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 10:49:43 +0200

    exercice 1.4 fait

---
+++ exercice1.4.php
@@ -0,0 +1,7 @@
+<?php
+$string = "ma variable de type string";
+$int = 20 ;
+$float = 12.5 ;
+$boolean = true;
+echo $string.',<br> ma variable de type integer: ' .$int. ',<br> ma variable de type float: ' .$float. ',<br> ma variable de type boolean: ' .$boolean;
+?>
